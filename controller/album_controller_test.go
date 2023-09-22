10a4bd99a4f536e6db3a23a2b123ab9238c7c5ee
坂本一弥 <sakamotokazuya@sakamotokazuyanoMacBook-Pro.local>
2023-09-22 21:14:24 +0900

    create testcode

---
+++ controller/album_controller_test.go
@@ -0,0 +1,102 @@
+package controller
+
+import (
+	"encoding/json"
+	"lgtm-kinako-api/model"
+	"lgtm-kinako-api/usecase"
+	"lgtm-kinako-api/usecase/mock"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+	"time"
+
+	"github.com/labstack/echo/v4"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestAlbumController_GetAllAlbums(t *testing.T) {
+    expectedAlbums := []model.AlbumResponse{
+        {
+            ID:        1,
+            Title:     "Album 1",
+            Image:     "image1.jpg",
+            CreatedAt: time.Now(),
+            UpdatedAt: time.Now(),
+        },
+    }
+
+    mockAlbumUsecase := new(mock.MockAlbumUsecase)
+    mockAlbumUsecase.On("GetAllAlbums").Return(expectedAlbums, nil)
+    controller := NewAlbumController(usecase.IAlbumUsecase(mockAlbumUsecase), nil)
+
+    e := echo.New()
+    req := httptest.NewRequest(http.MethodGet, "/albums", nil)
+    rec := httptest.NewRecorder()
+    c := e.NewContext(req, rec)
+
+    if assert.NoError(t, controller.GetAllAlbums(c)) {
+        assert.Equal(t, http.StatusOK, rec.Code)
+
+        var albums []model.AlbumResponse
+        err := json.Unmarshal(rec.Body.Bytes(), &albums)
+        assert.NoError(t, err)
+        assert.Len(t, albums, len(expectedAlbums), "Unexpected number of albums")
+
+		for i, expected := range expectedAlbums {
+			assert.Equal(t, expected.ID, albums[i].ID, "Unexpected album ID at index", i)
+			assert.Equal(t, expected.Title, albums[i].Title, "Unexpected album title at index", i)
+			assert.Equal(t, expected.Image, albums[i].Image, "Unexpected album image at index", i)
+		
+			// 時間に依存しないCreatedAtおよびUpdatedAtフィールドの比較
+			assert.True(t, expected.CreatedAt.Equal(albums[i].CreatedAt), "Unexpected album CreatedAt at index", i)
+			assert.True(t, expected.UpdatedAt.Equal(albums[i].UpdatedAt), "Unexpected album UpdatedAt at index", i)
+		}
+    }
+}
+
+func TestAlbumController_GetRandomAlbums(t *testing.T) {
+    expectedAlbums := []model.AlbumResponse{
+        {
+            ID:        1,
+            Title:     "Album 1",
+            Image:     "image1.jpg",
+            CreatedAt: time.Now(),
+            UpdatedAt: time.Now(),
+        },
+        {
+            ID:        2,
+            Title:     "Album 2",
+            Image:     "image2.jpg",
+            CreatedAt: time.Now(),
+            UpdatedAt: time.Now(),
+        },
+    }
+
+    mockAlbumUsecase := new(mock.MockAlbumUsecase)
+    mockAlbumUsecase.On("GetRandomAlbums").Return(expectedAlbums, nil)
+    controller := NewAlbumController(usecase.IAlbumUsecase(mockAlbumUsecase), nil)
+
+    e := echo.New()
+    req := httptest.NewRequest(http.MethodGet, "/random-albums", nil)
+    rec := httptest.NewRecorder()
+    c := e.NewContext(req, rec)
+
+    if assert.NoError(t, controller.GetRandomAlbums(c)) {
+        assert.Equal(t, http.StatusOK, rec.Code)
+
+        var albums []model.AlbumResponse
+        err := json.Unmarshal(rec.Body.Bytes(), &albums)
+        assert.NoError(t, err)
+
+        assert.Len(t, albums, len(expectedAlbums), "Unexpected number of albums")
+        for i, expected := range expectedAlbums {
+            assert.Equal(t, expected.ID, albums[i].ID, "Unexpected album ID at index", i)
+            assert.Equal(t, expected.Title, albums[i].Title, "Unexpected album title at index", i)
+            assert.Equal(t, expected.Image, albums[i].Image, "Unexpected album image at index", i)
+
+            assert.True(t, expected.CreatedAt.Equal(albums[i].CreatedAt), "Unexpected album CreatedAt at index", i)
+            assert.True(t, expected.UpdatedAt.Equal(albums[i].UpdatedAt), "Unexpected album UpdatedAt at index", i)
+        }
+    }
+}
+
--- go.mod
+++ go.mod
@@ -10,6 +10,7 @@ require (
 	github.com/joho/godotenv v1.5.1
 	github.com/labstack/echo-jwt/v4 v4.1.0
 	github.com/labstack/echo/v4 v4.11.1
+	github.com/stretchr/testify v1.8.1
 	golang.org/x/crypto v0.11.0
 	gorm.io/driver/postgres v1.5.2
 	gorm.io/gorm v1.25.4
@@ -17,6 +18,7 @@ require (
 
 require (
 	github.com/asaskevich/govalidator v0.0.0-20200108200545-475eaeb16496 // indirect
+	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/golang-jwt/jwt v3.2.2+incompatible // indirect
 	github.com/golang/freetype v0.0.0-20170609003504-e2365dfdc4a0 // indirect
 	github.com/jackc/pgpassfile v1.0.0 // indirect
@@ -25,9 +27,13 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/jmespath/go-jmespath v0.4.0 // indirect
+	github.com/kr/text v0.2.0 // indirect
 	github.com/labstack/gommon v0.4.0 // indirect
 	github.com/mattn/go-colorable v0.1.13 // indirect
 	github.com/mattn/go-isatty v0.0.19 // indirect
+	github.com/pmezard/go-difflib v1.0.0 // indirect
+	github.com/rogpeppe/go-internal v1.11.0 // indirect
+	github.com/stretchr/objx v0.5.0 // indirect
 	github.com/valyala/bytebufferpool v1.0.0 // indirect
 	github.com/valyala/fasttemplate v1.2.2 // indirect
 	golang.org/x/image v0.11.0 // indirect
@@ -35,4 +41,5 @@ require (
 	golang.org/x/sys v0.10.0 // indirect
 	golang.org/x/text v0.12.0 // indirect
 	golang.org/x/time v0.3.0 // indirect
+	gopkg.in/yaml.v3 v3.0.1 // indirect
 )
